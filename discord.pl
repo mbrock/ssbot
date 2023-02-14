d08f835e2aad9ae3a28772d654eaaf50ce8a6eec
Mikael Brockman <mikael@brockman.se>
2023-02-14 14:28:16 +0200

    work on discord thread concurrency stuff

--- discord.pl
+++ discord.pl
@@ -7,6 +7,15 @@
 :- use_module(apis).
 
 :- use_module(library(http/websocket)).
+:- use_module(library(spawn)).
+:- use_module(library(apply_macros)).
+
+:- debug(websocket).
+:- debug(websocket(_)).
+
+:- dynamic discord_sequence/1.
+
+discord_sequence(null).
 
 discord_gateway_url(URL) :-
     api_get(discord, ["gateway"], [], Result),
@@ -19,10 +28,6 @@ discord_opcode(9, invalid_session).
 discord_opcode(10, hello).
 discord_opcode(11, heartbeat_ack).
 discord_opcode(2, identify).
-discord_opcode(_, _) :- throw(error(bad)).
-
-:- debug(websocket).
-:- debug(websocket(_)).
 
 discord_gateway_send(Socket, Message) :-
     debug(websocket(send), "Sending ~p", [Message]),
@@ -30,23 +35,8 @@ discord_gateway_send(Socket, Message) :-
     save_event(send(websocket, discord, Message)).
 
 discord_gateway_send(Socket, Op, Data) :-
-    discord_opcode(Opcode, Op),
+    once(discord_opcode(Opcode, Op)),
     discord_gateway_send(Socket, _{op: Opcode, d: Data}).
-
-:- dynamic discord_sequence/1.
-
-discord_sequence(null).
-
-discord_heartbeat(Socket) :-
-    discord_sequence(Seq),
-    discord_gateway_send(Socket, heartbeat, Seq).
-
-discord_heartbeat_loop(Socket, Interval) :-
-    debug(websocket(heartbeat), "waiting ~p seconds", [Interval]),
-    sleep(Interval),
-    debug(websocket(heartbeat), "sending heartbeat", []),
-    discord_heartbeat(Socket),
-    discord_heartbeat_loop(Socket, Interval).
 
 discord_intents(1<<0, guilds).
 discord_intents(1<<1, guild_members).
@@ -70,11 +60,13 @@ discord_intent_bitmask(Intents, Bitmask) :-
     findall(X, (member(I, Intents), discord_intents(X, I)), Bits),
     sum_list(Bits, Bitmask).
 
+:- debug(event).
+
 discord_receive(Socket, Message) :-
     ws_receive(Socket, Payload, [format(json)]),
     Message = Payload.data,
     save_event(receive(websocket, discord, Message)),
-    discord_opcode(Op, Message.op),
+    discord_opcode(Message.op, Op),
     (  Op = dispatch
     -> retractall(discord_sequence(_)),
        asserta(discord_sequence(Message.s))
@@ -96,39 +88,54 @@ discord_gateway_close(Socket) :-
     ws_close(Socket, 1000, "Goodbye"),
     save_event(close(websocket, discord)).
 
-discord_gateway_connect(Intents, Ready, Socket) :-
-    discord_gateway_url(URL), !,
+discord_heartbeat_interval(Hello, Seconds) :-
+    Seconds is (Hello.d.heartbeat_interval / 1000).
+
+discord_heartbeat(Socket) :-
+    discord_sequence(Seq),
+    discord_gateway_send(Socket, heartbeat, Seq).
+
+% 1. receive hello
+% 2. start heartbeat
+% 3. send identify with intents
+% 4. receive ready
+% 5. start receiving
+% 6. be ready to send??
+%
+discord_heartbeat_loop(Socket, Interval) :-
+    debug(websocket(heartbeat), "waiting ~p seconds", [Interval]),
+    sleep(Interval),
+    debug(websocket(heartbeat), "sending heartbeat", []),
+    once(discord_heartbeat(Socket)),
+    discord_heartbeat_loop(Socket, Interval).
+
+discord_receive_loop(Socket) :-
+    once(discord_receive(Socket, _Message)),
+    discord_receive_loop(Socket).
+
+discord_session(Intents) :-
+    discord_gateway_url(URL),
     http_open_websocket(URL, Socket, []),
-    discord_receive(Socket, Hello), !,
-
-    HeartbeatInterval is Hello.d.heartbeat_interval / 1000,
-    thread_create(
-        discord_heartbeat_loop(Socket, HeartbeatInterval),
-        HeartbeatThread, []),
-
-    thread_at_exit(thread_signal(HeartbeatThread, thread_exit(bye))),
-
-    discord_heartbeat(Socket), !,
-    discord_receive(Socket, _Ack), !,
-    discord_identify(Socket, Intents), !,
-    discord_receive(Socket, Ready), !,
-    discord_loop(Socket).
-
-discord_loop(Socket) :-
-    discord_receive(Socket, _Message),
-    discord_loop(Socket).
-
-%% So, we have a websocket connection to Discord. Now what?
-%%
-%% We need to process the heartbeats, and we need to process
-%% the messages. We can do this with a thread that reads
-%% from the socket and puts the messages into a queue, and
-%% another thread that reads from the queue and processes
-%% the messages.
-%%
+    once(discord_receive(Socket, Hello)),
+    once(discord_heartbeat(Socket)),
+    discord_heartbeat_interval(Hello, Seconds),
+    async(discord_heartbeat_loop(Socket, Seconds), Heartbeat),
+    once(discord_identify(Socket, Intents)),
+    once(discord_receive(Socket, _Ready)),
+    async(discord_receive_loop(Socket), Receive),
+    writeln(waiting),
+    await(Heartbeat),
+    await(Receive).
 
 start(discord, Intents) :-
-    thread_create(discord_gateway_connect(Intents, _Ready, _Socket),
-                  _,
-                  [alias(discord),
-                   detached(true)]).
+    discord_session(Intents).
+
+
+
+
+
+
+
+
+
+
