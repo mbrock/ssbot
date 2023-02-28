e6fcc1429d41ce6f6294dc3c9567f6cb022372c6
Mikael Brockman <mikael@brockman.se>
2023-02-28 11:38:57 +0200

    wip

---
+++ make.pl
@@ -0,0 +1,7 @@
+:- module(make).
+
+%!  <module>  Rebuild external resources
+%   We implement a simple make-like system for rebuilding external
+%   resources.  This is used to rebuild the JavaScript with esbuild.
+
+
