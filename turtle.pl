09b3f51ce31a74fbe03d0cf2d6b7397c280a2799
Mikael Brockman <mikael@brockman.se>
2023-03-06 00:48:38 +0200

    <mbrock> M turtle.pl (+23/-25)

--- turtle.pl
+++ turtle.pl
@@ -1,7 +1,4 @@
-:- module(turtle_dcg, [turtle_clause/3,
-                       turtle_doc/3
-                      ]).
-
+:- module(turtle_dcg, [turtle_doc/3]).
 :- use_module(library(charsio), [char_type/2]).
 
 %% TODO: literals
@@ -38,27 +35,6 @@ turtle_iri(Prefix:Local) -->
     turtle_local(Local),
     spaces.
 
-%% Here are a bunch of details about the terminals from the spec,
-%% which we don't implement yet.
-%%
-%% PN_CHARS_BASE ::= [A-Z] | [a-z] | [#x00C0-#x00D6] | [#x00D8-#x00F6] |
-%%                   [#x00F8-#x02FF] | [#x0370-#x037D] | [#x037F-#x1FFF] |
-%%                   [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] |
-%%                   [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] |
-%%                   [#x10000-#xEFFFF]
-%% PN_CHARS_U    ::= PN_CHARS_BASE | '_'
-%% PN_CHARS      ::= PN_CHARS_U | '-' | [0-9] | #x00B7 | [#x0300-#x036F] |
-%%                   [#x203F-#x2040]
-%% PN_PREFIX     ::= PN_CHARS_BASE ((PN_CHARS | '.')* PN_CHARS)?
-%% PN_LOCAL      ::= (PN_CHARS_U | ':' | [0-9] | PLX)
-%%                   ((PN_CHARS | '.' | ':' | PLX)* (PN_CHARS | ':' | PLX))?
-%% PLX           ::= PERCENT | PN_LOCAL_ESC
-%% PERCENT       ::= '%' HEX HEX
-%% HEX           ::= [0-9] | [A-F] | [a-f]
-%% PN_LOCAL_ESC  ::= '\' ('_' | '~' | '.' | '-' | '!' | '$' | '&' | "'" |
-%%                        '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' |
-%%                        '?' | '#' | '@' | '%')
-
 alpha(C) --> [C], { char_type(C, alpha) }.
 
 turtle_prefix([C|Cs]) --> alpha(C), turtle_prefix(Cs).
@@ -92,3 +68,25 @@ turtle_object_list(S, P, [S-P-O|Rest]) -->
 
 turtle_object_list(S, P, [S-P-O]) -->
     turtle_object(O).
+
+
+%% Here are a bunch of details about the terminals from the spec,
+%% which we don't implement yet.
+%%
+%% PN_CHARS_BASE ::= [A-Z] | [a-z] | [#x00C0-#x00D6] | [#x00D8-#x00F6] |
+%%                   [#x00F8-#x02FF] | [#x0370-#x037D] | [#x037F-#x1FFF] |
+%%                   [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] |
+%%                   [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] |
+%%                   [#x10000-#xEFFFF]
+%% PN_CHARS_U    ::= PN_CHARS_BASE | '_'
+%% PN_CHARS      ::= PN_CHARS_U | '-' | [0-9] | #x00B7 | [#x0300-#x036F] |
+%%                   [#x203F-#x2040]
+%% PN_PREFIX     ::= PN_CHARS_BASE ((PN_CHARS | '.')* PN_CHARS)?
+%% PN_LOCAL      ::= (PN_CHARS_U | ':' | [0-9] | PLX)
+%%                   ((PN_CHARS | '.' | ':' | PLX)* (PN_CHARS | ':' | PLX))?
+%% PLX           ::= PERCENT | PN_LOCAL_ESC
+%% PERCENT       ::= '%' HEX HEX
+%% HEX           ::= [0-9] | [A-F] | [a-f]
+%% PN_LOCAL_ESC  ::= '\' ('_' | '~' | '.' | '-' | '!' | '$' | '&' | "'" |
+%%                        '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' |
+%%                        '?' | '#' | '@' | '%')
